8b69379d16763be88c0a3f946972e5c8e1ef6318
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-01-01 23:58:01 +0600

    v2 added

---
+++ Today's_tech_it/js/main.js
@@ -0,0 +1,48 @@
+// function message(){
+//     var Name = document.getElementById('studentName');
+//     var Email = document.getElementById('email');
+//     var Address = document.getElementById('address');
+//     var Mobile = document.getElementById('mobile');
+//     var Profession = document.getElementById('profession');
+//     var Subject = document.getElementById('subject');
+   
+//     var Batch = document.getElementById('batch');
+//     var Payment = document.getElementById('payment');
+    
+
+//     var Tx = document.getElementById('tx');
+   
+
+//     var Requirement = document.getElementById('requirement');
+
+//     const success = document.getElementById('success');
+//     const danger = document.getElementById('danger');
+
+//     if(Name.value === '' || Email.value === '' || Address.value === '' || Mobile.value === '' || Profession.value === '' || Subject.value === '' || Batch.value === '' || Payment.value === '' || Tx.value === '' ){
+//         danger.style.display = 'block'; 
+//     } 
+
+//     else{
+//         setTimeout(() => {
+//             Name.value = '';
+//             Email.value = '';
+//             Address.value = '';
+//             Mobile.value = '';
+//             Profession.value = '';
+//             Payment.value = '';
+            
+//             Tx.value = '';
+            
+//             Requirement.value = '';
+
+//         }, 2000);
+
+//         success.style.display = 'block';
+//     }
+//     setTimeout(() => {
+//         danger.style.display = 'none';
+//         success.style.display = 'none';
+//     }, 4000);
+// }
+
+
